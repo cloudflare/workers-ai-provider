117858e15fae4c4ea34614c3b4ed717c4ea5f0f4
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-02-17 19:23:17 +0800

    Fix: events should only be created once

--- packages/ai-provider/src/workersai-chat-language-model.ts
+++ packages/ai-provider/src/workersai-chat-language-model.ts
@@ -177,8 +177,6 @@ export class WorkersAIChatLanguageModel implements LanguageModelV1 {
   ): Promise<Awaited<ReturnType<LanguageModelV1["doStream"]>>> {
     const { args, warnings } = this.getArgs(options);
 
-    const decoder = new TextDecoder();
-
     const response = await this.config.binding.run(args.model, {
       messages: args.messages,
       stream: true,
@@ -188,48 +186,38 @@ export class WorkersAIChatLanguageModel implements LanguageModelV1 {
       throw new Error("This shouldn't happen");
     }
 
-    return {
-      stream: response.pipeThrough(
-        new TransformStream<
-          ParseResult<z.infer<typeof workersAIChatChunkSchema>>,
-          LanguageModelV1StreamPart
-        >({
-          async transform(chunk, controller) {
-            const chunkToText = decoder.decode(chunk as unknown as Uint8Array);
-            const chunks = events(new Response(chunkToText));
-            for await (const singleChunk of chunks) {
-              if (!singleChunk.data) {
-                continue;
-              }
-              if (singleChunk.data === "[DONE]") {
-                controller.enqueue({
-                  type: "finish",
-                  finishReason: "stop",
-                  usage: {
-                    promptTokens: 0,
-                    completionTokens: 0,
-                  },
-                });
-                return;
-              }
-              const data = JSON.parse(singleChunk.data);
+    const chunkEvent = events(new Response(response));
+    const usage = { promptTokens: 0, completionTokens: 0 };
 
+    return {
+      stream: new ReadableStream<LanguageModelV1StreamPart>({
+        async start(controller) {
+          for await (const event of chunkEvent) {
+            if (!event.data) {
+              continue;
+            }
+            if (event.data === "[DONE]") {
+              break;
+            }
+            const chunk = JSON.parse(event.data);
+            if (chunk.usage) {
+              usage.promptTokens = chunk.usage.prompt_tokens ?? 0;
+              usage.completionTokens = chunk.usage.completion_tokens ?? 0;
+            }
+            chunk.response.length &&
               controller.enqueue({
                 type: "text-delta",
-                textDelta: data.response ?? "DATALOSS",
+                textDelta: chunk.response,
               });
-            }
-            controller.enqueue({
-              type: "finish",
-              finishReason: "stop",
-              usage: {
-                promptTokens: 0,
-                completionTokens: 0,
-              },
-            });
-          },
-        })
-      ),
+          }
+          controller.enqueue({
+            type: "finish",
+            finishReason: "stop",
+            usage: usage,
+          });
+          controller.close();
+        },
+      }),
       rawCall: { rawPrompt: args.messages, rawSettings: args },
       warnings,
     };
